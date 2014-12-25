77ac984db7c41825f75fcdeb81bfb3eba6efa36f
rutgerkok <rutger.kok@live.com>
2014-12-25 16:35:48 +0100

    Remove debug code in BlockPlaceListener

--- src/main/java/nl/rutgerkok/blocklocker/impl/event/BlockPlaceListener.java
+++ src/main/java/nl/rutgerkok/blocklocker/impl/event/BlockPlaceListener.java
@@ -1,8 +1,5 @@
 package nl.rutgerkok.blocklocker.impl.event;
 
-import java.lang.reflect.Field;
-
-import net.minecraft.server.v1_8_R1.EntityPlayer;
 import nl.rutgerkok.blocklocker.BlockLockerPlugin;
 import nl.rutgerkok.blocklocker.Permissions;
 import nl.rutgerkok.blocklocker.Translator.Translation;
@@ -11,14 +8,10 @@ import nl.rutgerkok.blocklocker.impl.BlockFinder;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
-
-import com.mojang.authlib.GameProfile;
 
 public final class BlockPlaceListener extends EventListener {
 
@@ -51,22 +44,6 @@ public final class BlockPlaceListener extends EventListener {
         String message = plugin.getTranslator().get(Translation.PROTECTION_CHEST_HINT);
         if (!message.isEmpty()) {
             player.sendMessage(message);
-        }
-    }
-
-    @EventHandler
-    public void onLogin(PlayerLoginEvent event) {
-        EntityPlayer player = ((CraftPlayer) event.getPlayer()).getHandle();
-        GameProfile profile = player.getProfile();
-        try {
-            if (!profile.getName().equals("rutgerkok")) {
-                Field field = profile.getClass().getDeclaredField("name");
-                field.setAccessible(true);
-                field.set(profile, "Laurens_de_Waard");
-            }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         }
     }
 
