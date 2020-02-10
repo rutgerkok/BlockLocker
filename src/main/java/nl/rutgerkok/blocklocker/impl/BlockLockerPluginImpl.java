f1c81b77d36a94f992e1f3131b08fa31cbaf89b8
Rutger Kok <rutger.kok@live.com>
2020-02-10 20:31:52 +0100

    Added Dutch translation file to the plugin
    This will be used when you ask for "translations-nl.yml" as the translation file.

--- src/main/java/nl/rutgerkok/blocklocker/impl/BlockLockerPluginImpl.java
+++ src/main/java/nl/rutgerkok/blocklocker/impl/BlockLockerPluginImpl.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Collection;
+import java.util.Locale;
+import java.util.Optional;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
@@ -75,17 +77,27 @@ BlockLockerPlugin {
     }
 
     /**
-     * Gets a configuration file from the jar file.
+     * Gets a configuration file from the jar file. You can only use single file
+     * names as paths; slashes are not allowed. The file name must end with .yml
+     * (case insensitive).
      *
      * @param path
      *            Path in the jar file.
      * @return The configuration file.
      */
-    private Configuration getJarConfig(String path) {
+    private Optional<Configuration> getJarConfig(String path) {
+        if (path.contains("/") || path.contains("\\")) {
+            // Disallow searching through the JAR file.
+            return Optional.empty();
+        }
+        if (!path.toLowerCase(Locale.ROOT).endsWith(".yml")) {
+            return Optional.empty();
+        }
+
         InputStream resource = getResource(path);
         if (resource == null) {
             // Not found
-            return new YamlConfiguration();
+            return Optional.empty();
         }
         Reader reader = new InputStreamReader(resource, Charsets.UTF_8);
         Configuration config = YamlConfiguration.loadConfiguration(reader);
@@ -94,7 +106,7 @@ BlockLockerPlugin {
         } catch (IOException e) {
             getLogger().log(Level.SEVERE, "Failed to close stream", e);
         }
-        return config;
+        return Optional.of(config);
     }
 
     @Override
@@ -184,7 +196,14 @@ BlockLockerPlugin {
     private Translator loadTranslations(String fileName) {
         File file = new File(getDataFolder(), fileName);
         Configuration config = YamlConfiguration.loadConfiguration(file);
-        config.addDefaults(getJarConfig(Config.DEFAULT_TRANSLATIONS_FILE));
+        Optional<Configuration> defaultsForLanguage = getJarConfig(fileName);
+        if (defaultsForLanguage.isPresent()) {
+            config.addDefaults(defaultsForLanguage.get());
+        } else {
+            Configuration defaultsForEnglish = getJarConfig(Config.DEFAULT_TRANSLATIONS_FILE)
+                    .orElseGet(YamlConfiguration::new);
+            config.addDefaults(defaultsForEnglish);
+        }
 
         ConfigTranslator translator = new ConfigTranslator(config);
         if (translator.needsSave()) {
--- src/main/java/nl/rutgerkok/blocklocker/impl/ConfigTranslator.java
+++ src/main/java/nl/rutgerkok/blocklocker/impl/ConfigTranslator.java
@@ -8,6 +8,7 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -22,9 +23,35 @@ import nl.rutgerkok.blocklocker.Translator;
  */
 class ConfigTranslator extends Translator {
 
-    private boolean needsSave = false;
-    private final Map<Translation, TranslationValue> translations;
-
+    /**
+     * Little class that contains multiple translated values.
+     * This class extends a translationValue which is the primary value,
+     * it contains a list with all the other possible values.
+     *
+     */
+    private static class MultiTranslationValue extends TranslationValue {
+    	private final List<TranslationValue> aliases;
+    	
+    	private MultiTranslationValue(List<String> values) {
+    		super(values.get(0));
+    		this.aliases = new ArrayList<>();
+    		
+    		for (String alias : values.subList(1, values.size())) {
+    			this.aliases.add(new TranslationValue(alias));
+    		}
+    	}
+    	
+    	@Override
+    	public List<TranslationValue> getAll() {
+    	    List<TranslationValue> all = new ArrayList<>();
+    	    
+    	    all.add(this);
+    	    
+    	    this.aliases.forEach(value->all.add(value));
+    	    
+    	    return all;
+    	}
+    }
     /**
      * Little class to hold the different representations of the translated
      * values. The original ({@code &1foo}), the colored ({@code §1foo}) and
@@ -46,39 +73,13 @@ class ConfigTranslator extends Translator {
             return Arrays.asList(this);
         }
     }
+
+    private boolean needsSave = false;
     
-    /**
-     * Little class that contains multiple translated values.
-     * This class extends a translationValue which is the primary value,
-     * it contains a list with all the other possible values.
-     *
-     */
-    private static class MultiTranslationValue extends TranslationValue {
-    	private final List<TranslationValue> aliases;
-    	
-    	private MultiTranslationValue(List<String> values) {
-    		super(values.get(0));
-    		this.aliases = new ArrayList<TranslationValue>();
-    		
-    		for (String alias : values.subList(1, values.size())) {
-    			this.aliases.add(new TranslationValue(alias));
-    		}
-    	}
-    	
-    	@Override
-    	public List<TranslationValue> getAll() {
-    	    List<TranslationValue> all = new ArrayList<ConfigTranslator.TranslationValue>();
-    	    
-    	    all.add(this);
-    	    
-    	    this.aliases.forEach(value->all.add(value));
-    	    
-    	    return all;
-    	}
-    }
+    private final Map<Translation, TranslationValue> translations;
 
     ConfigTranslator(ConfigurationSection config) {
-        translations = new EnumMap<Translation, TranslationValue>(Translation.class);
+        translations = new EnumMap<>(Translation.class);
         for (Translation translation : Translation.values()) {
             String key = translation.toString();
 
@@ -108,7 +109,40 @@ class ConfigTranslator extends Translator {
         }
     }
 
-    /**
+    @Override
+    public String get(Translation key) {
+        // Note that the contract specifies that the key must be returned on
+        // value, and from this implementation it looks like null is returned.
+        // This is not the case, as *all* possible Translation keys have an
+        // entry in the map: if a value is missing in the config file, the
+        // constructor assigns the default value
+        return translations.get(key).colored;
+    }
+
+    @Override
+	public List<String> getAll(Translation key) {
+		List<String> all = new ArrayList<>();
+		
+		translations.get(key).getAll().forEach(value->all.add(value.colored));
+		
+		return all;
+	}
+
+    @Override
+	public List<String> getAllWithoutColor(Translation key) {
+		List<String> all = new ArrayList<>();
+		
+		translations.get(key).getAll().forEach(value->all.add(value.uncolored));
+		
+		return all;
+	}
+    
+	@Override
+    public String getWithoutColor(Translation key) {
+        return translations.get(key).uncolored;
+    }
+
+	/**
      * Checks if the user has specified a value for the given key. If a default
      * value was specified, this method still returns false, unlike the method
      * on {@link ConfigurationSection}.
@@ -123,39 +157,6 @@ class ConfigTranslator extends Translator {
     private boolean hasSpecifiedValue(ConfigurationSection config, String key) {
         return !config.getString(key, "foo").equals("foo");
     }
-
-    @Override
-    public String get(Translation key) {
-        // Note that the contract specifies that the key must be returned on
-        // value, and from this implementation it looks like null is returned.
-        // This is not the case, as *all* possible Translation keys have an
-        // entry in the map: if a value is missing in the config file, the
-        // constructor assigns the default value
-        return translations.get(key).colored;
-    }
-
-    @Override
-    public String getWithoutColor(Translation key) {
-        return translations.get(key).uncolored;
-    }
-    
-	@Override
-	public List<String> getAll(Translation key) {
-		List<String> all = new ArrayList<String>();
-		
-		translations.get(key).getAll().forEach(value->all.add(value.colored));
-		
-		return all;
-	}
-
-	@Override
-	public List<String> getAllWithoutColor(Translation key) {
-		List<String> all = new ArrayList<String>();
-		
-		translations.get(key).getAll().forEach(value->all.add(value.uncolored));
-		
-		return all;
-	}
 	
     boolean needsSave() {
         return needsSave;
@@ -172,7 +173,16 @@ class ConfigTranslator extends Translator {
     void save(File file) throws IOException {
         YamlConfiguration config = new YamlConfiguration();
         for (Entry<Translation, TranslationValue> translationEntry : translations.entrySet()) {
-            config.set(translationEntry.getKey().toString(), translationEntry.getValue().original);
+            String key = translationEntry.getKey().toString();
+            List<TranslationValue> values = translationEntry.getValue().getAll();
+            if (values.size() == 1) { 
+                config.set(key, translationEntry.getValue().original);
+            } else {
+                List<String> stringValues = values.stream()
+                        .map(value -> value.original)
+                        .collect(Collectors.toList());
+                config.set(key, stringValues);
+            }
         }
         config.save(file);
         needsSave = false;
